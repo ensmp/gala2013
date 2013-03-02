9b7b09d5d6b07a1d5f648644bb2cce2f8c25cff5
Edouard Leurent <eleurent@gmail.com>
2013-03-02 15:04:21 +0100

    titre en image, changement d'icones, petites modif de mise en forme

--- contenus/videos.php
+++ contenus/videos.php
@@ -1,5 +1,5 @@
 <h1> Trailer </h1>
-<p>Mettre la vidéo du trailer ici</p>
+<p><em>Coming soon</em></p>
 
 <h1> Concours Vidéos </h1>
 <p>Tu veux participer au concours vidéos ? Il te suffit de faire une vidéo de 30s autour d’une « chorée » sur la musique Riverside MotherFucker et sur <a href="#tuto">ce modèle</a>. Sois original, drôle ou impressionant pour gagner le concours !Et pour gagner quoi ? Les auteurs des trois vidéos les plus plébiscités se verront offrir :•	Trois places gratuites pour le Gala•	Trois places sur la tyrolienne que nous installons pour vous les neufs chanceux qui pourront l’utiliser ! Vous vous élancerez du balcon pour atterrir au beau milieu du dancefloor ! Le tout filmé et suivi par une poursuite !</p>
