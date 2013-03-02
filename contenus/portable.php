f0ebc18f425958d91a1a62fdfa2a57279964fd52
Edouard Leurent <eleurent@gmail.com>
2013-03-02 16:34:27 +0100

    icones dans les content

--- contenus/portable.php
+++ contenus/portable.php
@@ -1,3 +1,10 @@
 <h1> Contacts </h1>
 
-<p>Si tu as des questions sur le Gala, tu trouveras ici les contacts idéaux qui résoudront tous tes problèmes :</p><p><strong>Ambroise COLLON</strong> Président du GalaBDE des Mines ParisTech 06 67 47 83 58 </p><p><strong>Camille LAÏLY</strong>Vice-présidente du GalaBDE des Mines ParisTech 07 70 64 09 70</p><p><strong>Annabelle SIBUÉ</strong>  Bureau du BDE des Mines ParisTech 06 26 27 29 13</p><p>Si tu as des questions à poser à l’équipe écris-nous sur :Adresse mail : <a href="gala@mines-paris.eu">gala@mines-paris.eu</a>Page Facebook : <a href="http://www.facebook.com/MineNightInParis">www.facebook.com/MineNightInParis</a></p>
+<p>Si tu as des questions sur le Gala, tu trouveras ici les contacts idéaux qui résoudront tous tes problèmes :</p><p><strong>Ambroise COLLON</strong><br/>
+<img align="middle" src = "./design/pc/images/contact.png"/>
+Président du Gala, BDE des Mines ParisTech<br/><img align="middle" src = "./design/pc/images/phone.png"/>
+06 67 47 83 58 </p><p><strong>Camille LAÏLY</strong><br/><img align="middle" src = "./design/pc/images/contact.png"/>
+Vice-présidente du Gala,BDE des Mines ParisTech <br/>
+<img align="middle" src = "./design/pc/images/phone.png"/>07 70 64 09 70</p><p><strong>Annabelle SIBUÉ</strong><br/><img align="middle" src = "./design/pc/images/contact.png"/>
+Bureau du BDE des Mines ParisTech <br/>
+<img align="middle" src = "./design/pc/images/phone.png"/>06 26 27 29 13</p><p>Si tu as des questions à poser à l’équipe écris-nous sur :Adresse mail : <a href="gala@mines-paris.eu">gala@mines-paris.eu</a>Page Facebook : <a href="http://www.facebook.com/MineNightInParis">www.facebook.com/MineNightInParis</a></p>
