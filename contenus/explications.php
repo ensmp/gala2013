b4c5dfd6108b631d512490384b277723c2d3f778
Sparadox <etienne.lafarge@gmail.com>
2013-03-03 21:07:12 +0100

    Refonte complète du système d'animations pour un meilleur fonctionnement (pas encore totalement parfait mais presque). Ajout d'un message d'accueil servant à aider l'utilisateur. Agrandissement de la zone d'interaction pour la tour eiffel. Correction de bugs mineurs. Doudou si tu pouvais t'occupper des contenus. Je m'occuperai de la musique plus tard dans la soirée si j'ai le temps. La bise geek!

---
+++ contenus/explications.php
@@ -0,0 +1,7 @@
+<h1>Bienvenue sur le site du Gala des Mines</h1>
+<p id="bienvenue"> Bienvenue sur le site du gala des Mines de Paris. Pour trouver les rubriques, cherche à interagir avec les personnages sur les côtés. Il te suffit de passer ta souris au dessus de certaines zones et de cliquer sur les objets que tu pourra trouver. Un indice : ton curseur change quand tu passes au dessus d'un objet cliquable. 
+</p>
+
+<p>
+<strong>Conseil :</strong> pour un fonctionnement optimal, déplace ta souris <strong>doucement</strong>.
+</p>
