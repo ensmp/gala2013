949084b9b03f17b6da7e1a24821f562c0d1c6755
Ambroise COLLON <ambroisecollon@maccollon.rezal-mdm.com>
2013-03-04 03:34:41 +0100

    Modifications des  contenus

--- contenus/preventes.php
+++
@@ -1,3 +0,0 @@
-<h1> Acheter ma place </h1>
-
-<p>Début des ventes : mercredi 6 mars</p>
--- contenus/tyrolienne.php
+++ contenus/tyrolienne.php
@@ -6,7 +6,7 @@
 
 <p>« Où je signe ? »</p>
 
-<p>Une fois que tu as acheté ta place, tu peux participer au grand tirage au sort pour gagner ton ticket en guest star sur la tyrolienne. A un jour et une heure précise qui seront communiqués sur le site et la page Facebook, viens sur cette page et rentre ton <strong>nom</strong>, ton <strong>prénom</strong> et l’<strong>adresse mail</strong> utilisée lors de l’achat de ta place. Si tu es dans les cent premiers à t’inscrire, tu seras peut-être parmi les deux chanceux tirés au sort le soir-même, et tu pourras t’élancer au-dessus de la foule en délire le soir du 19 avril ! Et si tu n’y es pas, il te suffira de te réinscrire au prochain tirage, il n’y a pas de limite de participation.</p>
+<p>Une fois que tu as acheté ta place, tu peux participer au grand tirage au sort pour gagner ton ticket en guest star sur la tyrolienne. A un jour et une heure précise qui seront communiqués sur le site et la page Facebook, viens sur cette page et rentre ton <strong>nom</strong>, ton <strong>prénom</strong> et l’<strong>adresse mail</strong> utilisée lors de l’achat de ta place. Tu auras deux heures pour t'inscrire et tu seras peut-être parmi les deux chanceux tirés au sort le soir-même, et tu pourras t’élancer au-dessus de la foule en délire le soir du 19 avril ! Et si tu n’y es pas, il te suffira de te réinscrire au prochain tirage, il n’y a pas de limite de participation.</p>
 
 <p>Prochaine date : *****di **/** à 20h : tiens-toi prêt !</p>
 
