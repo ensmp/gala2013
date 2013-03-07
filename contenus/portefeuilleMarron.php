10985aa60da71b267b6a186bd37214e1d55defed
Ambroise COLLON <ambroisecollon@maccollon.home>
2013-03-07 01:12:28 +0100

    Le dernier  shot des contenus

--- contenus/portefeuilleMarron.php
+++ contenus/portefeuilleMarron.php
@@ -1,6 +1,7 @@
+<h1>Infos pratiques</h1>
 <p>Vente en ligne à partir du 7 Mars</p>
 
-<ul>	<li style="margin:10px 0"><strong>Quand ?</strong> Le vendredi 19 avril, à partir de 23h30</li>	<li style="margin:10px 0"><strong>Où ?</strong> Au Bataclan, salle de spectacle et de soirée prestigieuse en plein cœur de Paris</li>	<li style="margin:10px 0"><strong>Comment ?</strong></li>
+<ul>	<li style="margin:10px 0"><strong>Accès</strong></li>
 	<ul style="list-style:none;">		<li><img align="middle" src = "./design/pc/images/metro.png" style="margin-right:10px"><img align="middle" src = "./design/pc/images/5.png"><img align="middle" src = "./design/pc/images/9.png"> Oberkampf</li>
 		<ul style="list-style:none;">		
 		<li><img align="middle" src = "./design/pc/images/8.png"> Filles du Calvaire</li>		</ul>
@@ -8,7 +9,7 @@
 		<ul style="list-style:none;">
 		<li><img align="top" src = "./design/pc/images/bus_96.jpg" style="width:30px;"> Oberkampf – Filles du Calvaire</li>
 		</ul>
-	</ul>	<li style="margin:10px 0"><strong>Combien ?</strong></li>
+	</ul>	<li style="margin:10px 0"><strong>Tarifs</strong></li>
 		<ul>
 			<li>Entrée :</li>
 			<ul>
@@ -19,5 +20,5 @@
 			<li>Vestiaire : 2€ manteaux, 4€ sacs/casques</li>
 			<li>Table VIP et bouteille de champagne : 70€</li>
 		</ul>		<li style="margin:10px 0"><strong>Et quand est ce que les mineurs passent chez toi ?</strong></li>
-	<table width="100%">	<tr>		<td>Polytechnique</td>		<td>--/--/--</td>		<td>Centrale Paris</td>		<td>--/--/--</td>	</tr>	<tr>		<td>AgroParisTech</td>		<td>--/--/--</td>		<td>ULM</td>		<td>--/--/--</td>	</tr>	<tr>		<td>Telecom ParisTech</td>			<td>--/--/--</td>		<td>Ponts et Chaussée</td>		<td>--/--/--</td>	</tr>	<tr>		<td>Supélec</td>		<td>--/--/--</td>		<td>HEC ParisTech</td>		<td>--/--/--</td>	</tr>	<tr>		<td>Arts et Métiers</td>		<td>--/--/--</td>		<td>ENSTA ParisTech</td>		<td>--/--/--</td>	</tr>	<tr>		<td>ISRP</td>		<td>--/--/--</td>		<td>ESPCI</td>		<td>--/--/--</td>	</tr>	<tr>		<td>Chimie Paris</td>		<td>--/--/--</td>		<td>Sorbonne</td>		<td>--/--/--</td>	</tr>	<tr>		<td>Assas</td>		<td>--/--/--</td>		<td>ESCP</td>		<td>--/--/--</td>	</tr>	<tr>		<td>ENSAE</td>			<td>--/--/--</td>		<td>ENS Cachan</td>		<td>--/--/--</td>	</tr>	<tr>		<td>ESTP</td>		<td>--/--/--</td>	</tr>	</table>
+	<!--<table width="100%">	<tr>		<td>Polytechnique</td>		<td>--/--/--</td>		<td>Centrale Paris</td>		<td>--/--/--</td>	</tr>	<tr>		<td>AgroParisTech</td>		<td>--/--/--</td>		<td>ULM</td>		<td>--/--/--</td>	</tr>	<tr>		<td>Telecom ParisTech</td>			<td>--/--/--</td>		<td>Ponts et Chaussée</td>		<td>--/--/--</td>	</tr>	<tr>		<td>Supélec</td>		<td>--/--/--</td>		<td>HEC ParisTech</td>		<td>--/--/--</td>	</tr>	<tr>		<td>Arts et Métiers</td>		<td>--/--/--</td>		<td>ENSTA ParisTech</td>		<td>--/--/--</td>	</tr>	<tr>		<td>ISRP</td>		<td>--/--/--</td>		<td>ESPCI</td>		<td>--/--/--</td>	</tr>	<tr>		<td>Chimie Paris</td>		<td>--/--/--</td>		<td>Sorbonne</td>		<td>--/--/--</td>	</tr>	<tr>		<td>Assas</td>		<td>--/--/--</td>		<td>ESCP</td>		<td>--/--/--</td>	</tr>	<tr>		<td>ENSAE</td>			<td>--/--/--</td>		<td>ENS Cachan</td>		<td>--/--/--</td>	</tr>	<tr>		<td>ESTP</td>		<td>--/--/--</td>	</tr>	</table>-->
 </ul>
